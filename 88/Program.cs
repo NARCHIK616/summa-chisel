30a4430efdcf1bb2ad99029fce53ec04a91a1cdc
narek <narek@DESKTOP-98JS365>
2024-06-19 19:16:10 +0300

    Добавьте файлы проекта.

---
+++ 88/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int number1 = 5;
+        int number2 = 7;
+        int sum = AddNumbers(number1, number2);
+        Console.WriteLine($"Сумма чисел {number1} и {number2} равна {sum}");
+    }
+
+    static int AddNumbers(int a, int b)
+    {
+        return a + b;
+    }
+}
